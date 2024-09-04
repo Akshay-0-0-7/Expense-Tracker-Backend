940174e261ce282b4d204ce6f9ca5a62445cac52
Akshay.Uppu <Akshay.Uppu@ust.com>
2024-09-04 08:10:56 +0530

    Added Login and Registration Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './Components/Forms/login/login.component';
+import { RegisterComponent } from './Components/Forms/register/register.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
