0a1b33c2f61dabe756871dd9b505a04e9d4edb6e
Akshay.Uppu <Akshay.Uppu@ust.com>
2024-09-05 10:43:32 +0530

    added validations to formas and added navbar

--- src/app/Components/Forms/login/login.component.ts
+++ src/app/Components/Forms/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm:FormGroup
+  constructor(private fb:FormBuilder){
+    this.loginForm = this.fb.group({
+      // username: ['',[Validators.required]],
+      email: ['',[Validators.required,Validators.email]],
+      password: ['',[Validators.required,Validators.minLength(6)]]
+    })
+  }
 
+  onSubmit(){
+    console.log(this.loginForm.value.username)
+    console.log(this.loginForm.value.email)
+    console.log(this.loginForm.value.password)
+  }
+
+  validateControl(input:string){
+    return this.loginForm.get(input)?.invalid &&
+    (this.loginForm.get(input)?.touched ||
+    this.loginForm.get(input)?.dirty)
+  }
+
+  validateControlError(input:string,error:string){
+    return this.loginForm.get(input)?.hasError(error)
+    &&
+    (this.loginForm.get(input)?.touched ||
+    this.loginForm.get(input)?.dirty)
+  }
+
+get isFormValid() {
+  return this.loginForm.valid;
+}
 }
--- src/app/Components/Forms/register/register.component.ts
+++ src/app/Components/Forms/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,65 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm:FormGroup
+  constructor(private fb:FormBuilder){
+    this.registerForm = this.fb.group({
+      username: ['',[Validators.required]],
+      email: ['',[Validators.required,Validators.email]],
+      password: ['',[Validators.required,Validators.minLength(6)]],
+      confirmpassword: ['']
+    },
+    {
+      validator: this.passwordValidator()
+    })
+  }
 
+  onSubmit(){
+    console.log(this.registerForm.value.username)
+    console.log(this.registerForm.value.email)
+    console.log(this.registerForm.value.password)
+    console.log(this.registerForm.value.confirmpassword)
+  }
+
+  validateControl(input:string){
+    return this.registerForm.get(input)?.invalid &&
+    (this.registerForm.get(input)?.touched ||
+    this.registerForm.get(input)?.dirty)
+  }
+
+  validateControlError(input:string,error:string){
+    return this.registerForm.get(input)?.hasError(error)
+    &&
+    (this.registerForm.get(input)?.touched ||
+    this.registerForm.get(input)?.dirty)
+  }
+
+  // passwordValidator(control:AbstractControl){
+  //   var password = control.get('password')?.value
+  //   var confirmPassword = control.get('confirmpassword')?.value
+  //   if(password!==confirmPassword){
+  //     control.get('confirmpassword')?.setErrors({passwordMismatch:true})
+  //     return { passwordMismatch:true }
+  //   }
+  //   else{
+  //     return null
+  //   }
+  // }
+
+  passwordValidator(): ValidatorFn {
+    return (formGroup: AbstractControl): { [key: string]: boolean } | null => {
+      const password = formGroup.get('password')?.value;
+      const confirmPassword = formGroup.get('confirmpassword')?.value;
+      if (password !== confirmPassword) {
+        formGroup.get('confirmpassword')?.setErrors({ passwordMismatch: true });
+        return { passwordMismatch: true };
+      } else {
+        return null;
+      }
+    };
+}
+
+get isFormValid() {
+  return this.registerForm.valid;
+}
 }
